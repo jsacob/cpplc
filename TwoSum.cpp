de779c06c61707e4005791f588f31feb7061e38a
jacob <jacobmofficial@outlook.com>
2026-06-15 21:09:44 -0600

    init commit

---
+++ TwoSum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+std::vector<int> twoSum(std::vector<int> *nums, int target) {
+
+    std::map<int,int> map;
+
+    for(int i = 0; i < nums->size(); i++) {
+        int complement = target - (*nums)[i];
+        int currentNum = (*nums)[i];
+
+        if(map.count(complement)) {
+            return {map[complement], i};
+        };
+        map[currentNum] = i;
+    };
+    return{};
+};
+
+int main () {
+    
+    std::vector<int> vec = {2, 7, 11, 15};
+    std::vector<int> *ptr_vec = &vec;
+    int target = 9;
+
+    twoSum(ptr_vec, target);
+
+    return 0;
+}
