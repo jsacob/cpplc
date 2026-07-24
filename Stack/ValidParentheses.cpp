8f064873d08d45b0c4a4fb98369f9a056e807c17
jacob <jacobmofficial@outlook.com>
2026-07-23 22:27:17 -0600

    update

---
+++ Stack/ValidParentheses.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <stack>
+
+class Solution {
+public:
+    bool isValid(std::string s) {
+        std::stack<char> stack;
+
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            else {
+                if (stack.empty()) return false;
+
+                if (c == ')' && stack.top() != '(') return false;
+                if (c == '}' && stack.top() != '{') return false;
+                if (c == ']' && stack.top() != '[') return false;
+
+                stack.pop();
+            }
+        }
+
+        return stack.empty();
+    }
+};
