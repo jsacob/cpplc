e6af9d988bbc6749f711bdeada1455ffe7e2b0de
jacob <jacobmofficial@outlook.com>
2026-07-24 14:12:04 -0600

    i_hate_commit_messages

--- Stack/ValidParentheses.cpp
+++ Stack/ValidParentheses.cpp
@@ -1,26 +1,39 @@
+#include <iostream>
 #include <string>
 #include <stack>
+#include <unordered_map>
 
 class Solution {
 public:
     bool isValid(std::string s) {
         std::stack<char> stack;
 
+        std::unordered_map<char, char> closeToOpen = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
         for (char c : s) {
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
+            if (closeToOpen.count(c)) {
+                if (stack.empty() || stack.top() != closeToOpen[c]) {
+                    return false;
+                }
+                stack.pop();
             }
             else {
-                if (stack.empty()) return false;
-
-                if (c == ')' && stack.top() != '(') return false;
-                if (c == '}' && stack.top() != '{') return false;
-                if (c == ']' && stack.top() != '[') return false;
-
-                stack.pop();
+                stack.push(c);
             }
         }
 
         return stack.empty();
     }
 };
+
+int main() {
+    std::string s = "({[]})";
+    Solution solution;
+    std::cout << (solution.isValid(s) ? "true" : "false") << std::endl;
+
+    return 0;
+}
